5ae5f6b48ef37466b0e3bf31ae35e3114d906e06
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-10-27 20:16:39 +0000

    Subiendo modelo de dominio

---
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/dominio/Apuesta.java
@@ -0,0 +1,160 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ve.edu.ucab.ibet.dominio;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author nath
+ */
+@Entity
+@Table(name = "APUESTA")
+public class Apuesta implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected ApuestaPK apuestaPK;
+    @Basic(optional = false)
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Basic(optional = false)
+    @Column(name = "monto")
+    private double monto;
+    @Column(name = "gano")
+    private Boolean gano;
+    @Column(name = "empato")
+    private Boolean empato;
+    @JoinColumn(name = "username", referencedColumnName = "username", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Users users;
+    @JoinColumn(name = "idMedioPago", referencedColumnName = "id", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private MedioPago medioPago;
+    @JoinColumns({@JoinColumn(name = "idEvento", referencedColumnName = "idEvento"), @JoinColumn(name = "idParticipante", referencedColumnName = "idParticipante")})
+    @ManyToOne(optional = false)
+    private TableroGanancia tableroGanancia;
+
+    public Apuesta() {
+    }
+
+    public Apuesta(ApuestaPK apuestaPK) {
+        this.apuestaPK = apuestaPK;
+    }
+
+    public Apuesta(ApuestaPK apuestaPK, Date fecha, double monto) {
+        this.apuestaPK = apuestaPK;
+        this.fecha = fecha;
+        this.monto = monto;
+    }
+
+    public Apuesta(String username, int idMedioPago) {
+        this.apuestaPK = new ApuestaPK(username, idMedioPago);
+    }
+
+    public ApuestaPK getApuestaPK() {
+        return apuestaPK;
+    }
+
+    public void setApuestaPK(ApuestaPK apuestaPK) {
+        this.apuestaPK = apuestaPK;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public Boolean getGano() {
+        return gano;
+    }
+
+    public void setGano(Boolean gano) {
+        this.gano = gano;
+    }
+
+    public Boolean getEmpato() {
+        return empato;
+    }
+
+    public void setEmpato(Boolean empato) {
+        this.empato = empato;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public MedioPago getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(MedioPago medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public TableroGanancia getTableroGanancia() {
+        return tableroGanancia;
+    }
+
+    public void setTableroGanancia(TableroGanancia tableroGanancia) {
+        this.tableroGanancia = tableroGanancia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (apuestaPK != null ? apuestaPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Apuesta)) {
+            return false;
+        }
+        Apuesta other = (Apuesta) object;
+        if ((this.apuestaPK == null && other.apuestaPK != null) || (this.apuestaPK != null && !this.apuestaPK.equals(other.apuestaPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ve.edu.ucab.ibet.dominio.Apuesta[apuestaPK=" + apuestaPK + "]";
+    }
+
+}
---
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/dominio/UsuarioMedioPago.java
@@ -0,0 +1,151 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ve.edu.ucab.ibet.dominio;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author nath
+ */
+@Entity
+@Table(name = "USUARIO_MEDIO_PAGO")
+public class UsuarioMedioPago implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected UsuarioMedioPagoPK usuarioMedioPagoPK;
+    @Basic(optional = false)
+    @Column(name = "activo")
+    private boolean activo;
+    @Basic(optional = false)
+    @Column(name = "fechaInicio")
+    @Temporal(TemporalType.DATE)
+    private Date fechaInicio;
+    @Column(name = "fechaFin")
+    @Temporal(TemporalType.DATE)
+    private Date fechaFin;
+    @Basic(optional = false)
+    @Column(name = "montoMaximo")
+    private double montoMaximo;
+    @JoinColumn(name = "username", referencedColumnName = "username", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Users users;
+    @JoinColumn(name = "idMedioPago", referencedColumnName = "id", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private MedioPago medioPago;
+
+    public UsuarioMedioPago() {
+    }
+
+    public UsuarioMedioPago(UsuarioMedioPagoPK usuarioMedioPagoPK) {
+        this.usuarioMedioPagoPK = usuarioMedioPagoPK;
+    }
+
+    public UsuarioMedioPago(UsuarioMedioPagoPK usuarioMedioPagoPK, boolean activo, Date fechaInicio, double montoMaximo) {
+        this.usuarioMedioPagoPK = usuarioMedioPagoPK;
+        this.activo = activo;
+        this.fechaInicio = fechaInicio;
+        this.montoMaximo = montoMaximo;
+    }
+
+    public UsuarioMedioPago(String username, int idMedioPago) {
+        this.usuarioMedioPagoPK = new UsuarioMedioPagoPK(username, idMedioPago);
+    }
+
+    public UsuarioMedioPagoPK getUsuarioMedioPagoPK() {
+        return usuarioMedioPagoPK;
+    }
+
+    public void setUsuarioMedioPagoPK(UsuarioMedioPagoPK usuarioMedioPagoPK) {
+        this.usuarioMedioPagoPK = usuarioMedioPagoPK;
+    }
+
+    public boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getMontoMaximo() {
+        return montoMaximo;
+    }
+
+    public void setMontoMaximo(double montoMaximo) {
+        this.montoMaximo = montoMaximo;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public MedioPago getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(MedioPago medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (usuarioMedioPagoPK != null ? usuarioMedioPagoPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UsuarioMedioPago)) {
+            return false;
+        }
+        UsuarioMedioPago other = (UsuarioMedioPago) object;
+        if ((this.usuarioMedioPagoPK == null && other.usuarioMedioPagoPK != null) || (this.usuarioMedioPagoPK != null && !this.usuarioMedioPagoPK.equals(other.usuarioMedioPagoPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ve.edu.ucab.ibet.dominio.UsuarioMedioPago[usuarioMedioPagoPK=" + usuarioMedioPagoPK + "]";
+    }
+
+}
