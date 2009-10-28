ac78d22bad93cd817e754489c65aa1603d0af04b
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-10-28 06:14:38 +0000

    Aplicacion para realizar pruebas con hibernate en HQL

---
+++ branches/HibernateIbet/src/java/dominio/Categoria.java
@@ -0,0 +1,147 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dominio;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author nath
+ */
+@Entity
+@Table(name = "CATEGORIA")
+@NamedQueries({@NamedQuery(name = "Categoria.findAll", query = "SELECT c FROM Categoria c"), @NamedQuery(name = "Categoria.findById", query = "SELECT c FROM Categoria c WHERE c.id = :id"), @NamedQuery(name = "Categoria.findByNombre", query = "SELECT c FROM Categoria c WHERE c.nombre = :nombre"), @NamedQuery(name = "Categoria.findByEmpate", query = "SELECT c FROM Categoria c WHERE c.empate = :empate"), @NamedQuery(name = "Categoria.findByLogicaAutomatica", query = "SELECT c FROM Categoria c WHERE c.logicaAutomatica = :logicaAutomatica")})
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "empate")
+    private boolean empate;
+    @Basic(optional = false)
+    @Column(name = "logicaAutomatica")
+    private boolean logicaAutomatica;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCategoria")
+    private Collection<Evento> eventoCollection;
+    @OneToMany(mappedBy = "idCategoria")
+    private Collection<Categoria> categoriaCollection;
+    @JoinColumn(name = "idCategoria", referencedColumnName = "id")
+    @ManyToOne
+    private Categoria idCategoria;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id) {
+        this.id = id;
+    }
+
+    public Categoria(Integer id, String nombre, boolean empate, boolean logicaAutomatica) {
+        this.id = id;
+        this.nombre = nombre;
+        this.empate = empate;
+        this.logicaAutomatica = logicaAutomatica;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean getEmpate() {
+        return empate;
+    }
+
+    public void setEmpate(boolean empate) {
+        this.empate = empate;
+    }
+
+    public boolean getLogicaAutomatica() {
+        return logicaAutomatica;
+    }
+
+    public void setLogicaAutomatica(boolean logicaAutomatica) {
+        this.logicaAutomatica = logicaAutomatica;
+    }
+
+    public Collection<Evento> getEventoCollection() {
+        return eventoCollection;
+    }
+
+    public void setEventoCollection(Collection<Evento> eventoCollection) {
+        this.eventoCollection = eventoCollection;
+    }
+
+    public Collection<Categoria> getCategoriaCollection() {
+        return categoriaCollection;
+    }
+
+    public void setCategoriaCollection(Collection<Categoria> categoriaCollection) {
+        this.categoriaCollection = categoriaCollection;
+    }
+
+    public Categoria getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Categoria idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Categoria)) {
+            return false;
+        }
+        Categoria other = (Categoria) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dominio.Categoria[id=" + id + "]";
+    }
+
+}
---
+++ branches/HibernateIbet/src/java/dominio/UsuarioMedioPago.java
@@ -0,0 +1,152 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dominio;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author nath
+ */
+@Entity
+@Table(name = "USUARIO_MEDIO_PAGO")
+@NamedQueries({@NamedQuery(name = "UsuarioMedioPago.findAll", query = "SELECT u FROM UsuarioMedioPago u"), @NamedQuery(name = "UsuarioMedioPago.findByUsername", query = "SELECT u FROM UsuarioMedioPago u WHERE u.usuarioMedioPagoPK.username = :username"), @NamedQuery(name = "UsuarioMedioPago.findByIdMedioPago", query = "SELECT u FROM UsuarioMedioPago u WHERE u.usuarioMedioPagoPK.idMedioPago = :idMedioPago"), @NamedQuery(name = "UsuarioMedioPago.findByActivo", query = "SELECT u FROM UsuarioMedioPago u WHERE u.activo = :activo"), @NamedQuery(name = "UsuarioMedioPago.findByFechaInicio", query = "SELECT u FROM UsuarioMedioPago u WHERE u.fechaInicio = :fechaInicio"), @NamedQuery(name = "UsuarioMedioPago.findByFechaFin", query = "SELECT u FROM UsuarioMedioPago u WHERE u.fechaFin = :fechaFin"), @NamedQuery(name = "UsuarioMedioPago.findByMontoMaximo", query = "SELECT u FROM UsuarioMedioPago u WHERE u.montoMaximo = :montoMaximo")})
+public class UsuarioMedioPago implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected UsuarioMedioPagoPK usuarioMedioPagoPK;
+    @Basic(optional = false)
+    @Column(name = "activo")
+    private boolean activo;
+    @Basic(optional = false)
+    @Column(name = "fechaInicio")
+    @Temporal(TemporalType.DATE)
+    private Date fechaInicio;
+    @Column(name = "fechaFin")
+    @Temporal(TemporalType.DATE)
+    private Date fechaFin;
+    @Basic(optional = false)
+    @Column(name = "montoMaximo")
+    private double montoMaximo;
+    @JoinColumn(name = "idMedioPago", referencedColumnName = "id", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private MedioPago medioPago;
+    @JoinColumn(name = "username", referencedColumnName = "username", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Users users;
+
+    public UsuarioMedioPago() {
+    }
+
+    public UsuarioMedioPago(UsuarioMedioPagoPK usuarioMedioPagoPK) {
+        this.usuarioMedioPagoPK = usuarioMedioPagoPK;
+    }
+
+    public UsuarioMedioPago(UsuarioMedioPagoPK usuarioMedioPagoPK, boolean activo, Date fechaInicio, double montoMaximo) {
+        this.usuarioMedioPagoPK = usuarioMedioPagoPK;
+        this.activo = activo;
+        this.fechaInicio = fechaInicio;
+        this.montoMaximo = montoMaximo;
+    }
+
+    public UsuarioMedioPago(String username, int idMedioPago) {
+        this.usuarioMedioPagoPK = new UsuarioMedioPagoPK(username, idMedioPago);
+    }
+
+    public UsuarioMedioPagoPK getUsuarioMedioPagoPK() {
+        return usuarioMedioPagoPK;
+    }
+
+    public void setUsuarioMedioPagoPK(UsuarioMedioPagoPK usuarioMedioPagoPK) {
+        this.usuarioMedioPagoPK = usuarioMedioPagoPK;
+    }
+
+    public boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getMontoMaximo() {
+        return montoMaximo;
+    }
+
+    public void setMontoMaximo(double montoMaximo) {
+        this.montoMaximo = montoMaximo;
+    }
+
+    public MedioPago getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(MedioPago medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (usuarioMedioPagoPK != null ? usuarioMedioPagoPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UsuarioMedioPago)) {
+            return false;
+        }
+        UsuarioMedioPago other = (UsuarioMedioPago) object;
+        if ((this.usuarioMedioPagoPK == null && other.usuarioMedioPagoPK != null) || (this.usuarioMedioPagoPK != null && !this.usuarioMedioPagoPK.equals(other.usuarioMedioPagoPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dominio.UsuarioMedioPago[usuarioMedioPagoPK=" + usuarioMedioPagoPK + "]";
+    }
+
+}
