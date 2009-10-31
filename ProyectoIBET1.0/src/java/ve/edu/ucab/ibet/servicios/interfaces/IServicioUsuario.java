885f48d75743c03a9b5ff0f5479b7fc57a7b44f0
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-10-31 23:14:48 +0000

    - Maquetacion parcial del reporte de historial de los usuarios

--- ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/interfaces/IServicioUsuario.java
+++ ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/interfaces/IServicioUsuario.java
@@ -9,7 +9,12 @@ import ve.edu.ucab.ibet.generic.excepciones.GeneralException;
  * @version 1.0
  */
 public interface IServicioUsuario {
-
+    /**
+     * firma para el registro de un nuevo usuario
+     * @param user objeto usuario a registrar
+     * @return booleano con la condicion
+     * @throws GeneralException
+     */
     public boolean  registroNuevoUsuarioM (Users user) throws GeneralException;
 
 }
