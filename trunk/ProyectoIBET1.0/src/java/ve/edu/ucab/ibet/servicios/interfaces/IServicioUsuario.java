1ad328ad03466d261c6ac8f80b01fae2f54b3375
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-11-01 06:36:05 +0000

    - Servicios y prueba unitaria para registro de usuarios

--- trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/interfaces/IServicioUsuario.java
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/interfaces/IServicioUsuario.java
@@ -9,12 +9,19 @@ import ve.edu.ucab.ibet.generic.excepciones.GeneralException;
  * @version 1.0
  */
 public interface IServicioUsuario {
+
     /**
      * firma para el registro de un nuevo usuario
      * @param user objeto usuario a registrar
      * @return booleano con la condicion
      * @throws GeneralException
      */
-    public boolean  registroNuevoUsuarioM (Users user) throws GeneralException;
+    public boolean registroNuevoUsuarioM(Users user) throws GeneralException;
 
+    /**
+     * firma para verificar si existe un usuario
+     * @param u objeto usuario a verificar
+     * @return condicion booleana
+     */
+    public boolean existeUsuario(Users u);
 }
