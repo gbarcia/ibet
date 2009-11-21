97f83b7e7f67b19db1c8078ecc80d63dd551d57d
trujillo.jonathan <trujillo.jonathan@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-11-21 18:09:28 +0000

    Servicio de categoria

--- trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/impl/ServicioCategoriaImpl.java
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/impl/ServicioCategoriaImpl.java
@@ -1,5 +1,8 @@
 package ve.edu.ucab.ibet.servicios.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import ve.edu.ucab.ibet.dominio.Categoria;
 import ve.edu.ucab.ibet.generic.dao.interfaces.IGenericDao;
 import ve.edu.ucab.ibet.generic.util.helpers.interfaces.IHelperProperties;
 import ve.edu.ucab.ibet.servicios.interfaces.IServicioCategoria;
@@ -28,5 +31,26 @@ public class ServicioCategoriaImpl implements IServicioCategoria {
 
     public void setHelperProp(IHelperProperties helperProp) {
         this.helperProp = helperProp;
+    }
+
+    public List<Categoria> obtenerCategorias() {
+        List<Categoria> todasLasCategorias = new ArrayList<Categoria>();
+        todasLasCategorias.addAll(genericDao.listar(Categoria.class));
+
+        List<Categoria> categoriasPadre = new ArrayList<Categoria>();
+        for (Categoria categoria : todasLasCategorias) {
+            if (categoria.getIdCategoria() == null) {
+                categoriasPadre.add(categoria);
+            }
+        }
+        return categoriasPadre;
+    }
+
+    public List<Categoria> obtenerSubcategoriasDeUnaCategoria(Categoria categoriaPadre) {
+        List<Categoria> subcategorias = new ArrayList<Categoria>();
+        String query = "select c.categoriaCollection from Categoria c where c.id = ?";
+        Object[] parametros = {categoriaPadre.getId()};
+        subcategorias.addAll(this.genericDao.ejecutarQueryList(query, parametros));
+        return subcategorias;
     }
 }
--- trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/interfaces/IServicioCategoria.java
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/servicios/interfaces/IServicioCategoria.java
@@ -1,10 +1,17 @@
 package ve.edu.ucab.ibet.servicios.interfaces;
 
+import java.util.List;
+import ve.edu.ucab.ibet.dominio.Categoria;
+
 /**
  * Servicio para la gestion de categorias
  * @author Gerardo Barcia
- * @version 1.o
+ * @version 1.0
  */
 public interface IServicioCategoria {
+
+    public List<Categoria> obtenerCategorias();
+
+    public List<Categoria> obtenerSubcategoriasDeUnaCategoria(Categoria categoriaPadre);
 
 }
