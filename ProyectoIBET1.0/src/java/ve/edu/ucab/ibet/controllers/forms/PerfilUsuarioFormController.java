c698c3f99d779c6b77aa4a6b7e9778e58904282c
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-11-28 00:45:57 +0000

    -Arreglos de internacionalizacion y presentacion para gestion de perfil de usuarios y tablero de apuesta

--- ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/controllers/forms/PerfilUsuarioFormController.java
+++ ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/controllers/forms/PerfilUsuarioFormController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ve.edu.ucab.ibet.controllers.forms;
 
 import java.security.Principal;
