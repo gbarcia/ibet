d3b7cde66fefce8aa9fb122d8f8a8200389de883
mayita.uribe <mayita.uribe@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-12-12 21:47:29 +0000

    - Modificacion en el metodo para obtener las ganancias por usuario. (ahora si se obtiene lo que se gano en la apuesta)

--- trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/dominio/to/reportes/GananciasPorUsuarioTO.java
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/dominio/to/reportes/GananciasPorUsuarioTO.java
@@ -11,14 +11,20 @@ public class GananciasPorUsuarioTO {
     private Double montoGanado;
     private String nombreEvento;
     private Date fechaEvento;
+    private Boolean gano;
+    private Double proporcionGano;
+    private Double proporcionEmpate;
 
     public GananciasPorUsuarioTO() {
     }
 
-    public GananciasPorUsuarioTO(Double montoGanado, String nombreEvento, Date fechaEvento) {
+    public GananciasPorUsuarioTO(Double montoGanado, String nombreEvento, Date fechaEvento, Boolean gano, Double proporcionGano, Double proporcionEmpate) {
         this.montoGanado = montoGanado;
         this.nombreEvento = nombreEvento;
         this.fechaEvento = fechaEvento;
+        this.gano = gano;
+        this.proporcionGano = proporcionGano;
+        this.proporcionEmpate = proporcionEmpate;
     }
 
     public Date getFechaEvento() {
@@ -43,6 +49,30 @@ public class GananciasPorUsuarioTO {
 
     public void setNombreEvento(String nombreEvento) {
         this.nombreEvento = nombreEvento;
+    }
+
+    public Boolean getGano() {
+        return gano;
+    }
+
+    public void setGano(Boolean gano) {
+        this.gano = gano;
+    }
+
+    public Double getProporcionEmpate() {
+        return proporcionEmpate;
+    }
+
+    public void setProporcionEmpate(Double proporcionEmpate) {
+        this.proporcionEmpate = proporcionEmpate;
+    }
+
+    public Double getProporcionGano() {
+        return proporcionGano;
+    }
+
+    public void setProporcionGano(Double proporcionGano) {
+        this.proporcionGano = proporcionGano;
     }
 
 }
