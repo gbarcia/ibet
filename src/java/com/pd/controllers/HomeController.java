babf691b495aab0e85d11e31f880d2bce87e6a08
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-12-20 17:12:35 +0000

    -Pruebas cliente ws de ubet

--- src/java/com/pd/controllers/HomeController.java
+++ src/java/com/pd/controllers/HomeController.java
@@ -12,19 +12,28 @@ import com.pd.generic.util.GenerarReporteDisco;
 import com.pd.generic.util.HelperProperties.HelperProperties;
 import com.pd.generic.util.mail.interfaces.IMailService;
 import com.pd.servicios.interfaces.IServicioCliente;
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.TimeZone;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.datatype.Duration;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.namespace.QName;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 /**
@@ -70,36 +79,61 @@ public class HomeController implements Controller {
 
 
 
+        
 
         try { // Call Web Service Operation
-            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service service = new ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service();
-            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices port = service.getIbetWebServicesPort();
+            _211._22._168._192._1234.Ubet service = new _211._22._168._192._1234.Ubet();
+            _211._22._168._192._1234.UbetSoap port = service.getUbetSoap12();
             // TODO initialize WS operation arguments here
-            java.lang.String fecha = "28/11/2009";
-            java.lang.String equipoUno = "Villarreal";
-            java.lang.String equipoDos = "Sporting Gijon";
+            java.lang.String fecha = "01/12/2009";
+            java.lang.String equipo1 = "Leones del Caracas";
+            java.lang.String equipo2 = "Navegantes del Magallanes";
             // TODO process result here
-            ve.edu.ucab.ibet.servicios.impl.ws.RespuestaProporcionWS result = port.consultarProporcionEvento(fecha, equipoUno, equipoDos);
-            System.out.println("Result = "+result.getEquipoUno());
-            System.out.println(result.getProporcionEquipoUno());
+            _211._22._168._192._1234.Respuesta result = port.consultarProporcionEvento(fecha, equipo1, equipo2);
+            if (result != null) {
+            System.out.println("Result = "+result.getParticipante1());
+            System.out.println(result.getParticipante2());
+            System.out.println(result.getProporcion1());
+            System.out.println(result.getProporcion2());
+            }
+            else {
+                System.out.println("vino nulo");
+            }
         } catch (Exception ex) {
-           ex.printStackTrace();
+            ex.printStackTrace();
         }
 
 
-        try { // Call Web Service Operation
-            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service service = new ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service();
-            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices port = service.getIbetWebServicesPort();
-            // TODO initialize WS operation arguments here
-            java.lang.String fecha = "28/11/2009";
-            java.lang.String participanteUno = "Villarreal";
-            java.lang.String participanteDos = "Sporting Gijon";
-            // TODO process result here
-            java.lang.String result = port.consultarResultadoEvento(fecha, participanteUno, participanteDos);
-            System.out.println("Result = "+result);
-        } catch (Exception ex) {
-            // TODO handle custom exceptions here
-        }
+
+//        try { // Call Web Service Operation
+//            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service service = new ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service();
+//            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices port = service.getIbetWebServicesPort();
+//            // TODO initialize WS operation arguments here
+//            java.lang.String fecha = "28/11/2009";
+//            java.lang.String equipoUno = "Villarreal";
+//            java.lang.String equipoDos = "Sporting Gijon";
+//            // TODO process result here
+//            ve.edu.ucab.ibet.servicios.impl.ws.RespuestaProporcionWS result = port.consultarProporcionEvento(fecha, equipoUno, equipoDos);
+//            System.out.println("Result = "+result.getEquipoUno());
+//            System.out.println(result.getProporcionEquipoUno());
+//        } catch (Exception ex) {
+//           ex.printStackTrace();
+//        }
+//
+//
+//        try { // Call Web Service Operation
+//            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service service = new ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices_Service();
+//            ve.edu.ucab.ibet.servicios.impl.ws.IbetWebServices port = service.getIbetWebServicesPort();
+//            // TODO initialize WS operation arguments here
+//            java.lang.String fecha = "28/11/2009";
+//            java.lang.String participanteUno = "Villarreal";
+//            java.lang.String participanteDos = "Sporting Gijon";
+//            // TODO process result here
+//            java.lang.String result = port.consultarResultadoEvento(fecha, participanteUno, participanteDos);
+//            System.out.println("Result = "+result);
+//        } catch (Exception ex) {
+//            // TODO handle custom exceptions here
+//        }
 
 
         Principal p = arg0.getUserPrincipal();
