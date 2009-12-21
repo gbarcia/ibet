d10811f114a9f32bcad20feae4b2e8a1865d8bb8
gerardobarciap <gerardobarciap@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2009-12-21 22:25:02 +0000

    - Inicio de la presentacion del admin, para colocar listas de eventos y su gestion

--- trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/controllers/views/IndexAdminController.java
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/controllers/views/IndexAdminController.java
@@ -1,9 +1,12 @@
 package ve.edu.ucab.ibet.controllers.views;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import ve.edu.ucab.ibet.dominio.Evento;
+import ve.edu.ucab.ibet.servicios.interfaces.IServicioEvento;
 
 /**
  * Controlador de presentacion para el index de la administracion
@@ -12,8 +15,19 @@ import org.springframework.web.servlet.mvc.Controller;
  */
 public class IndexAdminController implements Controller {
 
+    private IServicioEvento servicioEvento;
+
+    public IServicioEvento getServicioEvento() {
+        return servicioEvento;
+    }
+
+    public void setServicioEvento(IServicioEvento servicioEvento) {
+        this.servicioEvento = servicioEvento;
+    }
+
     public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        return new ModelAndView("privado/back/admin");
+        List<Evento> listaEventos = servicioEvento.todosLosEventos();
+        return new ModelAndView("privado/back/admin","listaEventos",listaEventos);
     }
 
 }
