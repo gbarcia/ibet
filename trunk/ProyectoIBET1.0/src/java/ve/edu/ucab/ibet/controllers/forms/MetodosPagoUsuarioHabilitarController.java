ccc8b421501afc6a724808f7185a413120d44753
trujillo.jonathan <trujillo.jonathan@2aa2ebde-b5b9-11de-a433-7fce7b4d64d6>
2010-01-27 04:45:43 +0000

    metodos de pago del usuario

---
+++ trunk/ProyectoIBET1.0/src/java/ve/edu/ucab/ibet/controllers/forms/MetodosPagoUsuarioHabilitarController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ve.edu.ucab.ibet.controllers.forms;
+
+/**
+ *
+ * @author jonathan
+ */
+public class MetodosPagoUsuarioHabilitarController {
+
+}
